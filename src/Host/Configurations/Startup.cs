ac35623484528e1ca05b5b97e9ef9a5157f2b989
phi <phi.kieu.insenco@gmail.com>
2022-12-12 17:14:07 +0700

    [TASK] On Wroking

---
+++ src/Host/Configurations/Startup.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Client.Configurations
+{
+    public static class Startup
+    {
+        internal static ConfigureHostBuilder AddConfigurations(this ConfigureHostBuilder host)
+        {
+            host.ConfigureAppConfiguration((context, config) =>
+            {
+                const string configurationsDirectory = "Configurations";
+                var env = context.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"{configurationsDirectory}/database.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"{configurationsDirectory}/database.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            });
+            return host;
+        }
+    }
+}
